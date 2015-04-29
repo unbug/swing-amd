f9666362c82d47454084773d2e57c0ae62e35415
unbug <tidelgl@gmail.com>
2015-04-29 10:45:24 +0800

    remove console.log

--- card.js
+++ card.js
@@ -12,8 +12,6 @@ define(function(require, exports, module) {
         util = {},
         _isTouchDevice;
 
-    console.log(Hammer);
-
     /**
      * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
      */
